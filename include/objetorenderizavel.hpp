027b3740d2388b2925d6407bccdfb3721c6b7e22
theolara272727 <theo.duarte2012@gmail.com>
2025-06-19 13:59:49 -0300

    Ajustes na hitbox do personagem e Documentação dos métodos do ObjetoRenderizavele Personagem

--- include/objetorenderizavel.hpp
+++ include/objetorenderizavel.hpp
@@ -11,16 +11,26 @@ class ObjetoRenderizavel{
         ALLEGRO_BITMAP* object_bitmap;
         float posX, posY,centerX,centerY;
     public:
+        //Construtor padrão com coordenadas e BITMAP associados
         ObjetoRenderizavel(float x,float y,ALLEGRO_BITMAP* bitmap);
         virtual ~ObjetoRenderizavel(){};
+        //Retorna posição de X
         float get_posX();
+        //Retorna posição de Y
         float get_posY();
+        //Retorna posição de X do centro da imagem, relativo ao bitmap
         float get_centerX();
+        //Retorna posição de Y do centro da imagem, relativo ao bitmap
         float get_centerY();
+        //Retorna o bitmap associado ao Objeto
         ALLEGRO_BITMAP* get_bitmap();
+        //Define a posição X
         void set_posX(float x);
+        //Define a posição Y
         void set_posY(float y);
+        //Renderiza a imagem nas coordenadas (x,y)
         virtual void render_object();
+        //Comportamento a cada tick padrão
         virtual void on_tick(){};
 };
 
