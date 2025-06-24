84ef12f3f5d6e4c330bee8dd28bce3fd148211c5
Marney Santos de Melo <marney.melo@outlook.com>
2025-06-24 14:57:08 -0300

    Corrige comentario

--- src/main.cpp
+++ src/main.cpp
@@ -299,7 +299,7 @@ int main() {
 
         // // --- Seção de Lógica de MENUS (Bloqueante) ---
         if (current_state == GameState::LOGIN || current_state == GameState::MAIN_MENU || current_state == GameState::PAUSED || current_state == GameState::GAMEOVER) {
-        // Exibi tela de GAMEOVERS
+        // Exibe tela de GAMEOVERS
             if (current_state == GameState::GAMEOVER) {
                 interfaces.mostrarGameOver(score_font, score_da_partida);
              }
