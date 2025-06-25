5ff0609da049a97ab7aa9311c6605831cd5f6507
rafael <rafaelaugustoresendemm@gmail.com>
2025-06-24 23:51:00 -0300

    finalizando briefs

--- include/coletavel.hpp
+++ include/coletavel.hpp
@@ -22,22 +22,38 @@ class Coletavel : public ObjetoRenderizavel{
     Coletavel(int posX, int posY, ALLEGRO_BITMAP* sprite, int velocidadeX, TiposColetaveis tipo);
     void set_coletado(bool aux);
     bool get_coletado();
-    //Retorna qual o tipo de coletável
+    /**
+     * @brief Retorna qual o tipo de coletável
+    */
     TiposColetaveis get_tipo();
-    //Move o coletável
+    /** 
+    * @brief Move o coletável
+    */
     void mover_coletavel();
-    //Atualiza a posição
+    /** 
+     * @brief Move a posição
+    */
     void on_tick() override;
-    //Verifica colisão
-    bool checkCollision(Hitbox other_hitbox);
-    //Verifica se o coletável está apto a ser deletado
+    /**
+     * @brief Verifica colisão
+    */
+     bool checkCollision(Hitbox other_hitbox);
+    /**
+     * @brief Verifica se o coletável está apto a ser deletado
+    */
     bool remover_coletavel();
 };
-//Limpa o vetor que contém os coletáveis, seja porque saíram da tela ou já foram coletados
+/**
+ * @brief Limpa o vetor que contém os coletáveis, seja porque saíram da tela ou já foram coletados
+ */
 void limpando_coletaveis(vector<Coletavel*>& coletaveis);
-//Decide qual será o coletável a ser spawnado, com a chance de ser Nenhum
+/**
+ * Decide qual será o coletável a ser spawnado, com a chance de ser Nenhum
+ */
 TiposColetaveis sortear_powerup();
-//Realiza a construção do coletável, dada as opções que temos
+/**
+ * @brief Realiza a construção do coletável, dada as opções que temos
+ */
 void construir_coletavel(vector<Coletavel*>& coletaveis, int posX, int posY, int velocidadeX,
     ALLEGRO_BITMAP* sprite_invincible, ALLEGRO_BITMAP* sprite_plus_score);
 
--- include/randomizador.hpp
+++ include/randomizador.hpp
@@ -17,21 +17,33 @@ class Randomizador
 
     public:
     Randomizador(int min, int max);
-    //Gera um inteiro aleatório, com base na distribuição uniforme no intervalo (min, max);
+    /**
+     * @brief Gera um inteiro aleatório, com base na distribuição uniforme no intervalo (min, max);
+     */
     int valor_aleatorio();
-    //Define o valor de mínimo
+    /**
+     * @brief Define o valor de mínimo
+     */
     void set_min(int);
-    //Retorna o valor de mínimo
+    /** 
+     * @brief Retorna o valor de mínimo
+     */
     float get_min();
-    //Define o valor de máximo
+    /** 
+     * @brief Define o valor de máximo
+     */
     void set_max(int);
-    //Retorna o valor de máximo
+    /**
+     * Retorna o valor de máximo
+     */
     float get_max();
 
 };
 
-/*Define um inteiro aleatório, recebendo uma distribuição já definida, com máximos e mínimos.
-Usado para calcular alturas de spawn*/
+/**
+ * @briefDefine um inteiro aleatório, recebendo uma distribuição já definida, com máximos e mínimos.
+Usado para calcular alturas de spawn
+ */
 int definir_altura(Randomizador* rando);
 
 #endif
